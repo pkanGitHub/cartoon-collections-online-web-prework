57aa9c5bacaa7c5ee86eced4412735214904508e
Phyllis Kan <pyk9q7@gmail.com>
2019-06-24 00:28:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
 def roll_call_dwarves(array)
-  # array.each_with_index {|word, index| puts "#{index+1} #{word}" }
+  array.each_with_index {|word, index| puts "#{index+1} #{word}" }
 end
 
 def summon_captain_planet(array)
-    # array.collect {|word| word.capitalize + "!" }
+    array.collect {|word| word.capitalize + "!" }
 end
 
 def long_planeteer_calls(array)
-    # array.any? {|word| word.length > 4}
+    array.any? {|word| word.length > 4}
 end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  # array.find { |cheese| cheese_types.include?(cheese) }
+  array.find { |cheese| cheese_types.include?(cheese) }
 end
